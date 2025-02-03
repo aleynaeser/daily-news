a1626560eeec0020620591d8850993f2b47140c2
Aleyna Eser <aleynaeser345.ma@gmail.com>
2025-02-03 22:53:16 +0300

    refactor and improve layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      xs: { max: '575px' },
+      sm: { min: '575px', max: '991px' },
+      md: { max: '1199px' },
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        base: 'var(--base)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        accent: 'var(--accent)',
+        neutral: 'var(--neutral)',
+        light: 'var(--light)',
+        dark: 'var(--dark)',
       },
     },
   },
