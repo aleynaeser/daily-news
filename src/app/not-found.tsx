cf8ef5aa2d5768eb4796d1660aba981b7b2f0cf4
Aleyna Eser <aleynaeser345.ma@gmail.com>
2025-02-04 01:54:11 +0300

    refactor structure

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className='flex h-full items-center justify-center text-center text-lg'>
+    <div className='flex h-screen items-center justify-center text-center text-lg'>
       <strong>404</strong>&nbsp;PAGE NOT FOUND
     </div>
   );
