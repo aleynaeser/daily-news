245dc9b0ddce5107a0c4f5e5026e840e8e24eb15
Aleyna Eser <aleynaeser345.ma@gmail.com>
2025-02-17 23:03:52 +0300

    updated Logo component

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 
 export default function Logo() {
   return (
-    <div className='border-black-light font-atariClassic col-[1/2] flex flex-col items-center justify-center border-r p-6 text-lg'>
-      <div>DAILY</div>
+    <a href='/' className='border-black-light font-atariClassic col-[1/2] flex flex-col items-center justify-center border-r p-6 text-lg'>
+      <div className='w-fit'>DAILY</div>
 
-      <div className='flex items-center gap-2'>
+      <div className='flex items-center gap-1'>
         <span className='text-lg font-bold'>NEWS</span>
+    
         <div className='grid grid-cols-2 gap-0.5'>
-          <span className='bg-gray h-2 w-2'></span>
-          <span className='bg-yellow h-2 w-2'></span>
-          <span className='bg-magenta h-2 w-2'></span>
-          <span className='bg-blue h-2 w-2'></span>
+          <span className='bg-gray h-[6px] w-[6px]'></span>
+          <span className='bg-yellow h-[6px] w-[6px]'></span>
+          <span className='bg-magenta h-[6px] w-[6px]'></span>
+          <span className='bg-blue h-[6px] w-[6px]'></span>
         </div>
       </div>
-    </div>
+    </a>
   );
 }
